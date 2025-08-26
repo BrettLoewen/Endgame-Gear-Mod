220e65c8bab512c292d41364df63490810a2907d
Brett Loewen <brettzky007@gmail.com>
2025-08-26 13:30:15 -0500

    Initial commit

---
+++ src/main/java/com/supremetor/endgamegear/EndgameGearClient.java
@@ -0,0 +1,10 @@
+package com.supremetor.endgamegear;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndgameGearClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
