94df492927c008ad8ed8e3925df00a3906e727d1
Brett Loewen <58195618+BrettLoewen@users.noreply.github.com>
2025-08-27 16:53:32 -0500

    Add Elytra model to Dragonite Chestplate
    Added a feature renderer that draws the elytra model as an overlay layer to any chestplate that has the GLIDER component (with an exception for the vanilla Elytra item).
    
    * Elytra model added, but chestplate model partially broken
    * Fixed broken chestplate texture
    * Cleaned up armored elytra files
    * Changed elytra drawing approach to support armor trims
    * Made sure mobs can wear the armor
    * Cleaned up code and prevented double elytra rendering on base elytra item

--- src/main/java/com/supremetor/endgamegear/EndgameGearClient.java
+++ src/main/java/com/supremetor/endgamegear/EndgameGearClient.java
@@ -1,10 +1,31 @@
 package com.supremetor.endgamegear;
 
+import com.supremetor.endgamegear.entity.client.ArmoredElytraFeatureRenderer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.entity.state.BipedEntityRenderState;
+import net.minecraft.util.Identifier;
 
 public class EndgameGearClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        // Register the Armored Elytra Feature Renderer
+        LivingEntityFeatureRendererRegistrationCallback.EVENT.register(
+                (entityType, entityRenderer, registrationHelper, context) -> {
+                    // Only register for biped renderers (players, zombies, etc.)
+                    if (entityRenderer.getModel() instanceof BipedEntityModel<?>) {
+                        // Use the vanilla elytra texture for now
+                        Identifier wingTexture = Identifier.of("minecraft", "textures/entity/equipment/wings/elytra.png");
 
+                        @SuppressWarnings("unchecked")
+                        FeatureRendererContext<BipedEntityRenderState, BipedEntityModel<BipedEntityRenderState>> ctx =
+                                (FeatureRendererContext<BipedEntityRenderState, BipedEntityModel<BipedEntityRenderState>>) entityRenderer;
+
+                        registrationHelper.register(new ArmoredElytraFeatureRenderer<>(ctx, wingTexture));
+                    }
+                }
+        );
     }
 }
